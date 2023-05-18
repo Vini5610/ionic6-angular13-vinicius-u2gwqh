1a9d44e50ef7252607d17e840aff9ca65304ce5f
Vini5610 <79315178+Vini5610@users.noreply.github.com>
2023-05-18 08:36:46 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, VERSION } from '@angular/core';
+
+
+export class HomePageComponent {}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Ionic 6.2 Angular ' + VERSION.major;
+
+  constructor() {}
+
+  ionViewDidEnter() {}
+}
